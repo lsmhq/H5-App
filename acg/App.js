739f19d26540421781db9da4b58f5eb399472efd
daifang <784433957@qq.com>
2019-11-12 16:08:22 +0800

    rm acg

--- acg/App.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-import { View} from 'react-native';
-import Main from './src/container/Main';
-export default class App extends Component {
-  render() {
-    return (<Main/>);
-  }
-}
