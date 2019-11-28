b73a61e1101cf267ad4f098e8f7c7d3ebe2211a9
daifang <784433957@qq.com>
2019-11-28 09:35:14 +0800

    other

---
+++ BackControl/public/javascripts/objLength.js
@@ -0,0 +1,7 @@
+function getObjLen(obj){
+    let i = 0;
+    for(let j in obj) {
+        i++;
+    }
+    return i;
+}
--- BackControl/routes/main.js
+++ BackControl/routes/main.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const pg = require("pg");
-
 // var list = {
-
 // }
 var pgdb = new pg.Pool({
   host: '127.0.0.1',
