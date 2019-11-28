f56068e06c873f81e9c5face6f72967e16bcece9
daifang <784433957@qq.com>
2019-11-28 17:15:03 +0800

    login + register

--- BackControl/routes/login_back.js
+++ BackControl/routes/login_back.js
@@ -8,6 +8,7 @@ var pgdb = new pg.Pool({
   password: '147852369',
   database: 'ACG'
 });
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('login');
@@ -16,13 +17,11 @@ router.get('/', function(req, res, next) {
 router.post('/',function(req,res,next){
   let data = req.body;
   console.log(data);
-  res.setHeader('Content-Type','text/plain;charset=utf-8');
+  res.setHeader('Content-Type','text/html;charset=utf-8');
   if(getObjLen(data) === 2){
     console.log('登录验证');
-    pgdb.connect((error,client,done)=>{
       let sqlStr = 'SELECT username,password FROM users WHERE username=$1';
-      client.query(sqlStr,[data.username],(err,value) => {
-        done();
+      pgdb.query(sqlStr,[data.username],(err,value) => {
         // console.log(value.rows[0].password);
         if(value.rowCount > 0){
           if(value.rows[0].password === data.password){
@@ -38,31 +37,39 @@ router.post('/',function(req,res,next){
           res.redirect('/pages?page=main');
         }
       })
-    })
-  }
-});
-//注册提交
-router.post('/',function(req,res,next){
-  let data = req.body;
-  console.log(data);
-  res.setHeader('Content-Type','text/plain;charset=utf-8');
-  if(getObjLen(data) ===4){
+  }else if(getObjLen(data) === 3){
     console.log('注册提交');
-    pgdb.connect((error,client,done)=>{
-      let sqlStr = 'INSERT INTO users (email,userid,password) VALUES($1,$3,$2)';
-      client.query(sqlStr,[data.email,data.username,data.password],(err,value)=>{
-        done();
-        res.redirect('/');
-      });
-    });
+      let sqlStr_insert = 'INSERT INTO users (email,userid,password,username) VALUES($1,$2,$3,$4)';
+      let sqlStr_select = 'SELECT username,email FROM users WHERE username=$1 OR email=$2'; 
+      pgdb.query(sqlStr_select,[data.username,data.email],(err,val)=>{
+        console.log(val.rows[0]);
+        if (val.rowCount <= 0){
+            pgdb.query(sqlStr_insert,[data.email+'',strRandom(10),data.pwd[0]+'',data.username+''],(err,val1)=>{
+              if(val1.rowCount > 0)
+                res.render('success',{success:'注册成功'});
+          });
+        }else if(val.rows[0].username === data.username){
+          res.render('wrong',{error:'用户名已存在'});
+        }else if(val.rows[0].email === data.email){
+          res.render('wrong',{error:'邮箱已被注册过'});
+        }
+      })
   }
 });
-
 function getObjLen(obj){
   let i = 0;
   for(let j in obj) {
       i++;
   }
   return i;
+}
+
+function strRandom(j){
+  var str = 'ABCDEFGHIGKLMNOPQRSTUVWXYZabcdefghigklmnopqrstuvwxyz123456789';
+  var outStr = '';
+  for(let i =0;i<j;i++){
+    outStr += str[parseInt(Math.random()*str.length-1)];
+  }
+  return outStr;
 }
 module.exports = router;
