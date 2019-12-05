58725cc92576ae8087f307b8c62d056624fd8ba3
daifang <784433957@qq.com>
2019-12-05 19:03:18 +0800

    2.0

--- BackControl/routes/api.js
+++ BackControl/routes/api.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var pg = require('pg');
 var qs = require('querystring');
-var md5 = require('md5-node');
 //数据库基本配置  
 var pgdb = new pg.Pool({
     host: '127.0.0.1',
@@ -19,7 +18,7 @@ router.get('/main',(req,res,next)=>{
             if(err || val.rowCount < 0){
                 res.json({status:'1',data:'error'});
             }else{
-                res.json({status:'0',data:[{user_num:val.rowCount},{context_num:val1.rowCount}]});
+                res.json({status:'0',data:[{user_count:val.rowCount},{context_count:val1.rowCount}]});
             }
         });
     });
@@ -27,19 +26,19 @@ router.get('/main',(req,res,next)=>{
 
 router.get('/chapter',(req,res,next)=>{
     var params_obj = qs.parse(req.url.split('?')[1]);
-    if(params_obj.type === md5('a')){
+    if(params_obj.type == 'animation'){
         let sqlStr = `SELECT * FROM context WHERE contexttype = 'animation'`;
         lend(sqlStr,res);            
     }
-    else if(params_obj.type === md5('c')){
+    else if(params_obj.type == 'comic'){
         let sqlStr = `SELECT * FROM context WHERE contexttype = 'comic'`;
         lend(sqlStr,res);  
     }
-    else if(params_obj.type === md5('g')){
+    else if(params_obj.type == 'game'){
         let sqlStr = `SELECT * FROM context WHERE contexttype = 'game'`;
         lend(sqlStr,res); 
     }
-    else if(params_obj.type === md5('all')){
+    else if(params_obj.type == 'all'){
         let sqlStr = 'SELECT * FROM context';
         lend(sqlStr,res);
     }
@@ -50,22 +49,22 @@ router.get('/goods',(req,res,next)=>{
 });
 router.get('/orders',(req,res,next)=>{
     var params_obj = qs.parse(req.url.split('?')[1]);
-    if(params_obj.type === md5('all')){
+    if(params_obj.id === ('all')){
         let sqlStr = `SELECT * FROM ordercontent`;
         lend(sqlStr,res);
     }else{
         console.log(decodeURI(params_obj.type));
-        let sqlStr = `SELECT * FROM ordercontent WHERE userid = ${params_obj.type}`;
+        let sqlStr = `SELECT * FROM ordercontent WHERE userid = ${params_obj.id}`;
         lend(sqlStr,res);
     }
 });
 router.get('/person',(req,res,next)=>{
     var params_obj = qs.parse(req.url.split('?')[1]);
-    if(params_obj.type === md5('all')){
+    if(params_obj.id === ('all')){
         let sqlStr = `SELECT id,name,level,email FROM users`;
         lend(sqlStr,res);
     }else{
-        let sqlStr = `SELECT * FROM ordercontent WHERE id = ${params_obj.type}`;
+        let sqlStr = `SELECT * FROM ordercontent WHERE id = ${decodeURIComponent(params_obj.id)}`;
         lend(sqlStr,res);
     }
 });
@@ -75,27 +74,27 @@ router.get('/admin',(req,res,next)=>{
 });
 router.get('/talk',(req,res,next)=>{
     var params_obj = qs.parse(req.url.split('?')[1]);
-    if(params_obj.type === md5('all')){
+    if(params_obj.id === ('all')){
         let sqlStr = `SELECT * FROM evaluation`;
         lend(sqlStr,res);
     }else{
-        let sqlStr = `SELECT * FROM evaluation WHERE contentid = ${params_obj.type}`;
+        let sqlStr = `SELECT * FROM evaluation WHERE contentid = ${params_obj.id}`;
         lend(sqlStr,res);
     }
 });
 router.get('/goods',(req,res,next)=>{
     var params_obj = qs.parse(req.url.split('?')[1]);
-    if(params_obj.type === md5('all')){
+    if(params_obj.id === ('all')){
         let sqlStr = `SELECT * FROM market`;
         lend(sqlStr,res);
     }else{
-        let sqlStr = `SELECT * FROM market WHERE id = ${params_obj.type}`;
+        let sqlStr = `SELECT * FROM market WHERE id = ${params_obj.id}`;
         lend(sqlStr,res);
     }
 })
 router.post('/person',(req,res,next)=>{
-    let sign;
-    let work;
+    let data = req.body;
+    console.log(data);
     switch(sign){
         case DLE://不合格，DLE应该是字符串
             let sqlStr = `DELETE FROM users WHERE `;
@@ -104,7 +103,9 @@ router.post('/person',(req,res,next)=>{
             break;
     }
 })
+router.post('/chapter',(req,res,next)=>{
 
+});
 function lend(sqlStr,res){
     pgdb.query(sqlStr,[],(err,val)=>{
         if(err || val.rowCount < 0){
