64c4b8bcc97e596ddc6a77e205d87c34c25aa314
daifang <784433957@qq.com>
2019-12-10 15:40:40 +0800

    login

--- BackControl/routes/login.js
+++ BackControl/routes/login.js
@@ -30,17 +30,17 @@ router.post('/',function(req,res,next){
   console.log(data);
   if(data.type==='login'){
     console.log('登录验证');
-      let sqlStr = 'SELECT username,password,status FROM users WHERE username=$1';
+      let sqlStr = 'SELECT name,password,status FROM users WHERE name=$1';
       pgdb.query(sqlStr,[data.username],(err,value) => {
         // console.log(value.rows[0].password);
         if(err){
           res.send('db is error');
         }else{
           if(value.rowCount > 0){
-            if(value.rows[0].password === md5(data.password)&&value.rows[0].name===data.username&&value.rows[0].status==='已激活'){
+            if(value.rows[0].password == md5(data.password)&&value.rows[0].name==data.username&&value.rows[0].status==='已激活'){
               res.setHeader('Set-cookie',[`loginStatus=${md5('true')}`,`username=${new Buffer(encodeURIComponent(value.rows[0].name)).toString('base64')}`]);
               res.send('success');
-            }else if(value.rows[0].state==='未激活'){
+            }else if(value.rows[0].status==='未激活'){
               res.setHeader('Set-cookie',[`loginStatus=${md5('false')}`]);
               res.send('未激活账号');
             }else{
