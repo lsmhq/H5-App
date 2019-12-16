7759064c1304e3ff5004124488e9cb89e38c96c2
daifang <784433957@qq.com>
2019-12-16 15:41:20 +0800

    api

--- BackControl/routes/api.js
+++ BackControl/routes/api.js
@@ -235,7 +235,7 @@ router.post('/goods',(req,res,next)=>{
             del(sqlStr,res);
             break;
         }case 'insert':{
-            let sqlStr = `INSERT INTO market VALUES('${data.id}','${data.name}','${data.path||'?'}','${data.price}','${data.source||'？'}','${data.brand ||'?'}','${data.evaluation||'?'}','${data.collect}')`;
+            let sqlStr = `INSERT INTO market VALUES('${data.id}','${data.name}','${data.path||'/'}','${data.price}','${data.source||'0'}','${data.brand ||'0'}','${data.evaluation||'0'}','${data.collect}')`;
             insert(sqlStr,res);
             break;
         }case 'update':{
