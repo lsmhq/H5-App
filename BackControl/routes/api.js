60e1925153a3968fc315d4b8d9fa559d48070f65
daifang <784433957@qq.com>
2019-12-17 15:43:39 +0800

    api

--- BackControl/routes/api.js
+++ BackControl/routes/api.js
@@ -88,8 +88,8 @@ router.post('/chapter',(req,res,next)=>{
                 }
             }            
             console.log(imgtype);
-            sqlStr_insert = `INSERT INTO context VALUES('${id}','${data.contenttype}','${data.autherid}','${decodeURIComponent(atob(data.auther))}','/content/${data.contenttype}/${id}','0','0','0','0','${time}','${data.title}','/images/animation/${id}/0${imgtype}')`;
-            console.log(sqlStr_insert);
+            // sqlStr_insert = `INSERT INTO context VALUES('${id}','${data.contenttype}','${data.autherid}','${decodeURIComponent(atob(data.auther))}','/content/${data.contenttype}/${id}','0','0','0','0','${time}','${data.title}','/images/animation/${id}/0${imgtype}')`;
+            // console.log(sqlStr_insert);
             res.send(sqlStr_insert);
             break;
         }
