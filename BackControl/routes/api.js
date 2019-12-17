ded35a2e71c7f459799ceb0575e0f2a1bb452d94
daifang <784433957@qq.com>
2019-12-17 14:27:34 +0800

    api

--- BackControl/routes/api.js
+++ BackControl/routes/api.js
@@ -82,7 +82,7 @@ router.post('/chapter',(req,res,next)=>{
                     break;
                 }
             }
-            sqlStr = `INSERT INTO context VALUES('${id}','${data.contexttype}','${data.autherid}','${data.auther}','/content/${data.contexttype}/${id}.json','${data.good||'0'}','${data.visit||'0'}','${data.collect||'0'}','${data.evaluationnum||'0'}','${data.timetamp}','${data.title}','/images/animation/${id}/${0+imgtype}')`;
+            sqlStr = `INSERT INTO context VALUES('${id}','${data.contexttype}','${data.autherid}','${decodeURIComponent(atob(data.auther))}','/content/${data.contexttype}/${id}.json','${data.good||'0'}','${data.visit||'0'}','${data.collect||'0'}','${data.evaluationnum||'0'}','${data.timetamp}','${data.title}','/images/animation/${id}/${0+imgtype}')`;
             console.log(sqlStr);
             insert_context(sqlStr,res,imgtype,id,data);
             break;
