7803c1d1ec4bf33516ef5b810624cb5f2e7702c8
daifang <784433957@qq.com>
2019-12-17 20:48:13 +0800

    body-parser

--- BackControl/app.js
+++ BackControl/app.js
@@ -18,8 +18,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({ limit: '50mb', extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json({limit: '50mb'}));
