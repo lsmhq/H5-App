bf6993a386e7c79e2baa0c514349895ef3ab1aad
daifang <784433957@qq.com>
2019-12-19 23:08:59 +0800

    acg wiki

--- BackControl/routes/api.js
+++ BackControl/routes/api.js
@@ -261,8 +261,6 @@ router.post('/person',(req,res,next)=>{
             let imgData = Buffer.from(images,'base64');
             let path = __dirname.split('/');
             path.pop();
-            console.log(imgData);
-            console.log(img_type);
             switch (img_type) {
                 case 'jpg'||'JPG':{
                     imgtype = '.jpg';
@@ -279,7 +277,6 @@ router.post('/person',(req,res,next)=>{
                 }
             }         
             path = `${path.join('/')}/public/images/avatar/${data.id+imgtype}`;
-            console.log(path);
             let sqlStr = `UPDATE users SET avatarid='${data.id+imgtype}' WHERE id='${data.id}'`;
             fs.writeFile(path,imgData,(err)=>{
                 if(err){
