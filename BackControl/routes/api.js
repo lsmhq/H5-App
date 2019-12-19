064408e89f56a329f80394a8381135a02cb0f2e4
daifang <784433957@qq.com>
2019-12-19 20:57:59 +0800

    api

--- BackControl/routes/api.js
+++ BackControl/routes/api.js
@@ -255,13 +255,13 @@ router.post('/person',(req,res,next)=>{
             update(sqlStr,res);
             break;
         }case 'update_img':{
-            let img_type = data.images_type.split('/')[1];
-            let images = data.images.split(',')[1];
+            let img_type = data.images_type.split('/')[1].split(';')[0];//图片类型
+            let images = data.images.split(',')[1];//图片数据
             let imgtype;
             let imgData = Buffer.from(images,'base64');
             let path = __dirname.split('/');
             path.pop();
-            path = `${path.join('/')}/public/images/avatar/${data.id+imgtype}`;
+            
             switch (img_type) {
                 case 'jpg'||'JPG':{
                     imgtype = '.jpg';
@@ -277,6 +277,7 @@ router.post('/person',(req,res,next)=>{
                     break;
                 }
             }         
+            path = `${path.join('/')}/public/images/avatar/${data.id+imgtype}`;
             let sqlStr = `UPDATE users SET avatarid='${data.id+imgtype}' WHERE id='${data.id}'`;
             fs.writeFile(path,imgData,(err)=>{
                 if(err){
