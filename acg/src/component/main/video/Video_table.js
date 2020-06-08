371042c09e64be56689cf8a157a6110aab99c843
daifang <784433957@qq.com>
2020-06-08 21:04:38 +0800

    video

--- acg/src/component/main/video/Video_table.js
+++ acg/src/component/main/video/Video_table.js
@@ -64,6 +64,7 @@ class Form extends Component {
                         headers:{
                             'Content-Type': 'application/json'
                         },
+                        mode:'cors',
                         body:JSON.stringify(data)
                     }).then(req=>req.text()).then(data=>{
                         if(data=='success'){
